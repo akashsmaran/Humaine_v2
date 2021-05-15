a1dd4f17106b13d84f2ac2f7aea892a9c0ab767e
Vikranth Kanumuru <20688709+highskillzz@users.noreply.github.com>
2021-05-15 19:54:59 +0530

    modified for docker use

--- API/config/database.js
+++ API/config/database.js
@@ -3,8 +3,9 @@ const { Client } = require("pg");
 require("dotenv").config();
 const client = new Client({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  // host: process.env.DB_HOST_DOCKER || process.env.DB_HOST,
+  // For dev, use below
+  // host: process.env.DB_HOST,
+  host: process.env.DB_HOST_DOCKER || process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
