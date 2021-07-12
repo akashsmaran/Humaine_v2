c382f7aa340db6f18ad897958bb703c8f3c4f2ad
Vikranth Kanumuru <20688709+highskillzz@users.noreply.github.com>
2021-07-12 21:01:44 +0530

    Fixing the email issue of view engine

--- API/routes/index.js
+++ API/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Your account has been activated' });
+router.get("/", function (req, res, next) {
+  // res.render('index', { title: 'Your account has been activated' });
+  res.sendFile("index", { title: "Your account has been activated" });
 });
 
 module.exports = router;
